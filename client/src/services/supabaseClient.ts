0dfc73e213ba4e7183ab559efbb79c54f061c343
jeancarlosdiaz <jeeancdd@gmail.com>
2025-07-28 15:00:11 -0500

    cambio de conexion

--- client/src/services/supabaseClient.ts
+++ client/src/services/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://vlmeifyldcgfmhppynir.supabase.co';
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZsbWVpZnlsZGNnZm1ocHB5bmlyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM0ODQwNDUsImV4cCI6MjA2OTA2MDA0NX0.8MtUi9I_evcJYvB3tXGCKsXDpUX7V13T_DDfBbRvvu8';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://clffvmueangquavnaokd.supabase.co';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNsZmZ2bXVlYW5ncXVhdm5hb2tkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM0NTgzMTcsImV4cCI6MjA2OTAzNDMxN30.NyUOwOMmJgpWqz5FRSV52EELCaEMkrHTLWd5JDw3ZiU';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey); 
